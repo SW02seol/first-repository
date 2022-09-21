709dd9274039a3809e1c11e34d434971a7b00757
SW02seol <112841773+SW02seol@users.noreply.github.com>
2022-09-21 17:10:50 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,10 @@
 def print_hello():
     print("Hello World!!")
 
+def print KIT():
+    print("Kumoh National
+Institute of Technology.")
+          
 if __name__ == '__main__':
+    print KIT()
     print_hello()
